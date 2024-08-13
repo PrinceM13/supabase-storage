01732b5ee180c2355699cf3f364ae50e4d43a0df
Parames Pasitprachaja <parames.pasitprachaja@gmail.com>
2024-08-13 14:56:30 +0700

    fix: typo error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import ImageUploadAndDisplay from "@/components/ImageUploadAndDisplay";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-10">
-      <h1>Supabse Storage</h1>
+      <h1>Supabase Storage</h1>
       <ImageUploadAndDisplay />
     </main>
   );
